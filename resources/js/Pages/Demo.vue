53d690432cb5872fc284d0ce6479fd7cc07dbbe2
morgan4080 <morganmurungi@live.com>
2021-09-20 07:12:39 +0300

    fixxing merge conflict

--- resources/js/Pages/Demo.vue
+++ resources/js/Pages/Demo.vue
@@ -9,7 +9,7 @@
                 <div class="bg-gray-100">
                     <div class="max-w-7xl mx-auto pt-32 pb-16 px-4 sm:pt-48 sm:pb-24 sm:px-6 lg:px-8">
                         <div class="relative bg-white shadow-xl">
-                            <h2 class="sr-only">Explore Demo Account</h2>
+                            <h2 class="sr-only">Contact us</h2>
 
                             <div class="grid grid-cols-1 lg:grid-cols-3">
                                 <!-- Contact information -->
@@ -48,7 +48,7 @@
                                         </svg>
                                     </div>
                                     <h3 class="text-lg font-medium text-white">Request Demo</h3>
-                                    <p class="mt-6 text-base text-blue-50 max-w-3xl">What Happens Next after you fill the form;</p>
+                                    <p class="mt-6 text-base text-blue-50 max-w-3xl">What Happens Next after you fill the form:</p>
                                     <dl class="mt-8 space-y-6">
                                         <dt></dt>
                                         <dd class="flex text-base text-blue-50">
