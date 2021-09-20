8730d271429b68a6d3e5b82654eb210b49bcea4c
morgan4080 <morganmurungi@live.com>
2021-09-20 06:47:21 +0300

    cicd:deploy1

--- server_deploy.sh
+++ server_deploy.sh
@@ -1,42 +1,22 @@
 #!/bin/sh
-
 set -e
 
 echo "Deploying application ..."
 
-sudo chmod -R 777 ./
-
-sudo chown -R www-data:www-data ./
-
-sudo usermod -a -G www-data ubuntu
-
 # Enter maintenance mode
-php artisan down --render="errors::503"
+(php artisan down) || true
 # Update codebase
 git fetch origin deploy
-
 git reset --hard origin/deploy
 #place correct env
 cp .env.ci .env
 # Install dependencies based on lock file
-sudo composer install --no-interaction --prefer-dist --optimize-autoloader
+composer install --no-interaction --prefer-dist --optimize-autoloader
 # Clear cache
 php artisan optimize
 # Reload PHP to update opcache
 echo "" | sudo -S service php7.4-fpm reload
 # Exit maintenance mode
 php artisan up
-# Link Storage
-php artisan storage:link
-
-sudo find /usr/share/laravel/ -type f -exec chmod 644 {} \;
-
-sudo find /usr/share/laravel/ -type d -exec chmod 755 {} \;
-
-sudo chown -R www-data:www-data /usr/share/laravel/
-
-sudo chgrp -R www-data storage bootstrap/cache
-
-sudo chmod -R ug+rwx storage bootstrap/cache
-
 echo "Application deployed!"
+
