21b59ddb2e15fd8e5e178c45288866ee63d8b7a4
morgan4080 <morganmurungi@live.com>
2021-09-20 07:37:54 +0300

    blade remove browser sync

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -71,10 +71,5 @@
     </head>
     <body class="font-sans antialiased">
         @inertia
-
-        @env ('local')
-            <script src="http://localhost:8000/browser-sync/browser-sync-client.js"></script>
-        @endenv
-
     </body>
 </html>
