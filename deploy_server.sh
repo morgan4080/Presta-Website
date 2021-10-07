2c84e67766a1d1c11d9e129264a49e0306354939
morgan4080 <morganmurungi@live.com>
2021-10-07 20:03:10 +0300

    refresh db

--- deploy_server.sh
+++ deploy_server.sh
@@ -12,6 +12,8 @@ git reset --hard origin/deploy
 cp .env.ci .env
 # Install dependencies based on lock file
 composer install --no-interaction --prefer-dist --optimize-autoloader
+# migrate fresh
+php artisan migrate:fresh
 # Clear cache
 php artisan optimize
 # Reload PHP to update opcache
