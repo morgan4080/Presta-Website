3f9af173ed8574bab32e329c20a5b123970b9756
morgan4080 <morganmurungi@live.com>
2021-10-07 20:10:20 +0300

    deployer

--- resources/js/Shared/FileInput.vue
+++ resources/js/Shared/FileInput.vue
@@ -41,7 +41,7 @@ export default {
   },
   methods: {
     filesize(size) {
-      var i = Math.floor(Math.log(size) / Math.log(1024))
+      let i = Math.floor(Math.log(size) / Math.log(1024))
       return (size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i]
     },
     browse() {
