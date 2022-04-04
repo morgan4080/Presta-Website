d6b751da8867725005f3473962bcd12fd1490f26
lennyjuma <lennyndungu34@gmail.com>
2022-04-04 11:25:27 +0300

    icon color set

--- resources/js/Pages/Gallery/Index.vue
+++ resources/js/Pages/Gallery/Index.vue
@@ -36,8 +36,8 @@
                             <Link :href="route('gallery.show',[album.id])">
                                 <div class="space-y-4 sm:flex-col">
                                     <div class="relative aspect-w-3 aspect-h-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="absolute icon-color top-0 p-2 rounded hover:bg-blue-800 h-16 w-16" viewBox="0 0 20 20" fill="currentColor">
-                                            <path fill-rule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clip-rule="evenodd" />
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="absolute icon-color top-0 p-2 rounded hover:icon-color h-16 w-16" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                                         </svg>
                                         <img class="object-cover shadow-lg rounded-lg hover:opacity-30" :src="album.gallery_image[0]" alt="">
                                     </div>
