3f8ea63b3237e8d21609d8952708fb5a96f3fadd
lennyjuma <lennyndungu34@gmail.com>
2022-04-05 10:41:36 +0300

    coming up

--- app/Http/Controllers/CaseStudyController.php
+++ app/Http/Controllers/CaseStudyController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CaseStudyController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('CaseStudy/Index', [
+
+        ]);
+    }
+    public function create()
+    {
+        return Inertia::render('CaseStudy/Create', [
+
+        ]);
+    }
+    public function show()
+    {
+        return Inertia::render('CaseStudy/View', [
+
+        ]);
+    }
+    public function edit()
+    {
+        return Inertia::render('CaseStudy/Edit', [
+
+        ]);
+    }
 }
