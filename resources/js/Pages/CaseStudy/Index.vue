f572740446a77a5fa9485754e7e3a8c097a2e212
lennyjuma <lennyndungu34@gmail.com>
2022-04-05 11:30:38 +0300

    case-study index coming up

--- resources/js/Pages/CaseStudy/Index.vue
+++ resources/js/Pages/CaseStudy/Index.vue
@@ -26,6 +26,30 @@
                 </a>
             </nav>
         </div>
+
+        <ul data-component class="grid grid-cols-2 gap-2 pt-4" style="--min: 22ch; --gap: 2rem;">
+            <li data-component class="smol-card-component">
+                <img src='https://images.unsplash.com/photo-1607274227313-08146c819e6f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=80&w=400' alt=''>
+                <h3>Card Headline 1</h3>
+                <p>Chocolate cake macaroon tootsie roll pastry gummies.</p>
+                <p>Apple pie jujubes cheesecake ice cream gummies sweet roll lollipop.</p>
+                <a href="https://moderncss.dev">Visit ModernCSS.dev</a>
+            </li>
+            <li data-component class="smol-card-component">
+                <img src='https://images.unsplash.com/photo-1607274227313-08146c819e6f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=80&w=400' alt=''>
+                <h3>Card Headline 1</h3>
+                <p>Chocolate cake macaroon tootsie roll pastry gummies.</p>
+                <p>Apple pie jujubes cheesecake ice cream gummies sweet roll lollipop.</p>
+                <a href="https://moderncss.dev">Visit ModernCSS.dev</a>
+            </li>
+            <li data-component class="smol-card-component">
+                <img src='https://images.unsplash.com/photo-1607274227313-08146c819e6f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=80&w=400' alt=''>
+                <h3>Card Headline 1</h3>
+                <p>Chocolate cake macaroon tootsie roll pastry gummies.</p>
+                <p>Apple pie jujubes cheesecake ice cream gummies sweet roll lollipop.</p>
+                <a href="https://moderncss.dev">Visit ModernCSS.dev</a>
+            </li>
+        </ul>
     </div>
     <MainFooter/>
 
