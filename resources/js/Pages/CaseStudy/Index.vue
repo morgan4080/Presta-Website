b38c410efc46c2d1e6fc902cce9484b353825ebf
lennyjuma <lennyndungu34@gmail.com>
2022-04-05 11:15:09 +0300

    case-study index coming up

--- resources/js/Pages/CaseStudy/Index.vue
+++ resources/js/Pages/CaseStudy/Index.vue
@@ -18,7 +18,7 @@
             <option v-for="tab in tabs" :key="tab.name" :selected="tab.current">{{ tab.name }}</option>
         </select>
     </div>
-    <div class="hidden sm:block mx-auto max-w-7xl">
+    <div class="hidden sm:block mx-auto max-w-7xl py-2">
         <div class="border-b border-gray-200">
             <nav class="-mb-px flex space-x-8" aria-label="Tabs">
                 <a v-for="tab in tabs" :key="tab.name" :href="tab.href" :class="[tab.current ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300', 'whitespace-nowrap uppercase py-4 px-1 border-b-2 font-medium text-sm']" :aria-current="tab.current ? 'page' : undefined">
