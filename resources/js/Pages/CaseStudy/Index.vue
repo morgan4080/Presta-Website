9a5794e2ed7185e899cd08feec0f48aafb3c603b
lennyjuma <lennyndungu34@gmail.com>
2022-04-05 11:08:06 +0300

    case-study index coming up

--- resources/js/Pages/CaseStudy/Index.vue
+++ resources/js/Pages/CaseStudy/Index.vue
@@ -7,7 +7,7 @@
             <img class="w-full h-full object-cover" src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&&sat=-100" alt="" />
             <div class="absolute inset-0 bg-indigo-800 mix-blend-multiply" aria-hidden="true" />
         </div>
-        <div class="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
+        <div class="relative max-w-7xl mx-auto py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
             <h1 class="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">Case Study</h1>
         </div>
     </div>
