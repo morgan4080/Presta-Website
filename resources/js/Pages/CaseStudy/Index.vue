e6ac38fb696c07b54673eee9220f044636938cc5
lennyjuma <lennyndungu34@gmail.com>
2022-04-06 15:39:13 +0300

    show page coming up

--- resources/js/Pages/CaseStudy/Index.vue
+++ resources/js/Pages/CaseStudy/Index.vue
@@ -3,7 +3,7 @@
     <NavigationHome :incomingNavClass="'bg-blue-presta4'" />
     <div class="relative bg-indigo-800 pt-32 px-4 sm:px-6 lg:px-8 lg:pt-40">
         <div class="absolute inset-0">
-            <img class="w-full h-full object-cover" src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&&sat=-100" alt="" />
+            <img class="w-full h-full object-cover" src="/images/logos/CaseStudy.png" alt="" />
             <div class="absolute inset-0 bg-indigo-800 mix-blend-multiply" aria-hidden="true" />
         </div>
         <div class="relative max-w-7xl mx-auto py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
