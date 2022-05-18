69d39763e98358ab3821e617d6146becf323b7fe
lennyjuma <lennyndungu34@gmail.com>
2022-05-18 15:22:36 +0300

    location

--- resources/js/Pages/Contact.vue
+++ resources/js/Pages/Contact.vue
@@ -48,7 +48,7 @@
                                         </svg>
                                     </div>
                                     <h3 class="text-lg font-medium text-white">Contact information</h3>
-                                    <p class="mt-6 text-base text-blue-50 max-w-3xl">KATO Place, Longonot Road Upperhill, Nairobi. Kenya.</p>
+                                    <p class="mt-6 text-base text-blue-50 max-w-3xl">Stone Groove, Mirema Road, Nairobi. Kenya.</p>
                                     <dl class="mt-8 space-y-6">
                                         <dt><span class="sr-only">Phone number</span></dt>
                                         <dd class="flex text-base text-blue-50">
