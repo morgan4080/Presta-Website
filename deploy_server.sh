8f22d6a67ec181893b287c2367457d61d37786ca
morgan4080 <morganmurungi@live.com>
2022-08-17 19:32:00 +0300

    prefer-source

--- deploy_server.sh
+++ deploy_server.sh
@@ -10,10 +10,8 @@ git fetch origin deploy
 git reset --hard origin/deploy
 #place correct env
 cp .env.ci .env
-# update dependencies
-composer update
 # Install dependencies based on lock file
-composer install --no-interaction --prefer-dist --optimize-autoloader
+composer install --no-interaction --prefer-source --optimize-autoloader
 # make migrations
 php artisan migrate
 # Clear cache
