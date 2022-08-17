07cbbc8775daf5caf85940883b372325760048f5
morgan4080 <morganmurungi@live.com>
2022-08-17 19:21:46 +0300

    few changes

--- resources/js/Pages/Confirmation.vue
+++ resources/js/Pages/Confirmation.vue
@@ -17,36 +17,6 @@
                 <div class="relative max-w-4xl lg:flex lg:items-center mx-auto">
 
                     <div class="hidden lg:block lg:flex-shrink-0">
-                        <img class="h-32 w-32 rounded-full" src="/images/ronoclose.png" alt="Quentin Afande Testimonial" />
-                    </div>
-
-                    <div class="relative lg:ml-6">
-                        <svg class="absolute top-24 left-0 transform -translate-x-8 -translate-y-24 h-16 w-16 text-blue-presta1 opacity-25" stroke="currentColor" fill="none" viewBox="0 0 144 144" aria-hidden="true">
-                            <path stroke-width="2" d="M41.485 15C17.753 31.753 1 59.208 1 89.455c0 24.664 14.891 39.09 32.109 39.09 16.287 0 28.386-13.03 28.386-28.387 0-15.356-10.703-26.524-24.663-26.524-2.792 0-6.515.465-7.446.93 2.327-15.821 17.218-34.435 32.11-43.742L41.485 15zm80.04 0c-23.268 16.753-40.02 44.208-40.02 74.455 0 24.664 14.891 39.09 32.109 39.09 15.822 0 28.386-13.03 28.386-28.387 0-15.356-11.168-26.524-25.129-26.524-2.792 0-6.049.465-6.98.93 2.327-15.821 16.753-34.435 31.644-43.742L121.525 15z" />
-                        </svg>
-                        <blockquote class="relative">
-                            <footer class="mt-8 text-lg leading-9 font-medium text-gray-900">
-                                <p>
-                                    It was awesome! I'm not ashamed of my body anymore. I recommend it to anyone who wants a personalized fitness program.
-                                </p>
-                            </footer>
-                            <div class="flex mt-4 justify-center md:justify-start">
-                                <div class="flex-shrink-0 lg:hidden">
-                                    <img class="h-12 w-12 rounded-full" src="/images/QuentinAfande.png" alt="" />
-                                </div>
-                                <div class="ml-4 lg:ml-0">
-                                    <div class="text-base font-medium text-gray-900">Quentin Afande</div>
-                                    <div class="text-base font-medium text-blue-presta4">MD. Feezy Mobile Credit Ltd.</div>
-                                </div>
-                            </div>
-                        </blockquote>
-                    </div>
-
-                </div>
-
-                <div class="relative max-w-4xl lg:flex lg:items-center mx-auto">
-
-                    <div class="hidden lg:block lg:flex-shrink-0">
                         <img class="h-32 w-32 rounded-full" src="/images/QuentinAfande.png" alt="Quentin Afande Testimonial" />
                     </div>
 
