ae9d4442a3c579f9f6acac0b81d696e1875e7b3d
morgan4080 <morganmurungi@live.com>
2022-08-24 14:58:01 +0300

    gtag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -27,6 +27,16 @@
             gtag('config', 'G-YSNM3NH1JV');
         </script>
 
+        <!-- Google Tag Manager -->
+        <script>
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K8GDVDP');
+        </script>
+        <!-- End Google Tag Manager -->
+
         <!-- Icons -->
         <link rel="apple-touch-icon" sizes="180x180" href="/images/icons/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="/images/icons/favicon-32x32.png">
@@ -67,6 +77,10 @@
         </style>
     </head>
     <body class="font-sans antialiased">
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K8GDVDP"
+                          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         @inertia
     </body>
 </html>
