cfd32d83e531792968c9effe47b12c4feea6ddf3
LindaKamere <82774464+lindakamere560@users.noreply.github.com>
2022-11-01 02:54:30 -0500

    v3 b4 merge

---
+++ resources/js/Components/LatestNews.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="bg-white mt-4 sm:mb-4">
+        <div class="mx-auto max-w-7xl flex flex-col justify-center items-center">
+            <h2 class="custom-blue-200 font-semibold text-sm">Get Our Application </h2>
+            <h1 class="sm:font-semibold text-3xl sm:text-center custom-blue-900">Latest News</h1>
+
+        </div>
+        <div class="mx-auto max-w-7xl grid grid-cols-3  sm:px-20 bg-white">
+            <div class="col-span-1 flex flex-col justify-centre items-center  sm:py-4 sm:px-4 ">
+                <img src="/images/LN1.png" alt="">
+
+                <div class="flex justify-center items-center">
+                    <svg width="39" height="3" viewBox="0 0 39 3" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <line y1="1.18848" x2="39" y2="1.18848" stroke="#FFAF2E" stroke-width="2"/>
+                    </svg>
+                    <h1 class=" sm:text-center custom-blue-dark sm:mt-6 mb-4">The Credit and Loan Collections Future. (Post COVID)
+                    </h1>
+                </div>
+                <div class="flex justify-center items-center">
+                    <p class="font-light sm:text-xs custom-grey-dark text-center pr-2">2 minute read</p>
+                    <svg width="10" height="8" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1.5625 4.18848H8.4375" stroke="#656464" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M5.625 1.37598L8.4375 4.18848L5.625 7.00098" stroke="#656464" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+
+                </div>
+
+            </div>
+            <div class="col-span-1 flex flex-col justify-center items-center  sm:px-4 ">
+                <img src="/images/LN2.png" alt="">
+
+                <div class="flex justify-center items-center">
+                    <svg width="39" height="3" viewBox="0 0 39 3" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <line y1="1.18848" x2="39" y2="1.18848" stroke="#FFAF2E" stroke-width="2"/>
+                    </svg>
+                    <h1 class="sm:text-center custom-blue-dark sm:mt-6 mb-4">How Can Lenders Accelerate Their Digital Transformation</h1>
+                </div>
+
+                <div class="flex justify-center items-center">
+                    <p class="font-light sm:text-xs custom-grey-dark text-center pr-2">2 minute read</p>
+                    <svg width="10" height="8" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1.5625 4.18848H8.4375" stroke="#656464" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M5.625 1.37598L8.4375 4.18848L5.625 7.00098" stroke="#656464" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+
+                </div>
+            </div>
+            <div class="col-span-1 flex flex-col justify-center items-center  sm:py-4 sm:px-4">
+                <img src="/images/LN3.png" alt="">
+                <div class="flex justify-center items-center">
+                    <svg width="39" height="3" viewBox="0 0 39 3" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <line y1="1.18848" x2="39" y2="1.18848" stroke="#FFAF2E" stroke-width="2"/>
+                    </svg>
+
+                    <h1 class="  sm:text-center custom-blue-dark sm:mt-6 mb-4">Meeting Post-Covid Lending Demands
+                    </h1>
+                </div>
+                <div class="inline-flex justify-center items-center">
+                    <p class="font-light sm:text-xs custom-grey-dark text-center pr-2">2 minute read</p>
+                    <svg class="inline-flex" width="10" height="8" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1.5625 4.18848H8.4375" stroke="#656464" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M5.625 1.37598L8.4375 4.18848L5.625 7.00098" stroke="#656464" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LatestNews.vue"
+}
+</script>
+
+<style scoped>
+
+.custom-grey-dark{
+    color:#656464;
+}
+
+.custom-blue-200{
+    color: #217BF4;
+}
+.custom-blue-900 {
+
+    color: #0A093D;
+}
+</style>
---
+++ resources/js/Components/Transform.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class="bg-white mt-4">
+        <div class="mx-auto max-w-7xl flex flex-col justify-center items-center">
+            <h2 class="custom-blue-200 my-4 font-semibold">TRANSFORMING THE SME CUSTOMER EXPERIENCE </h2>
+            <h1 class="sm:font-semibold text-4xl sm:text-center custom-blue-900">Opening the Doors of Opportunity </h1>
+            <p class=" sm:text-base custom-grey-dark text-center py-4">Master Loan Origination Challenges with Real Results that can make you ready for<br>
+                <span class="font-semibold">Today and the future </span>
+                </p>
+        </div>
+        <div class="mx-auto max-w-7xl grid grid-cols-2 sm:gap-x-8 sm:pt-12 sm:px-8 bg-blue-white">
+            <div class="col-span-1 ">
+                <img src="/images/LO3.png" alt="Prestatab" class="object-cover object-center group-hover:opacity-75 rounded-lg my-auto">
+            </div>
+
+            <div class="col-span-1 flex-col justify-between my-auto">
+                <p class=" my-4 tracking-wider text-2xl sm:py-4 custom-blue-200">What’s in it for your <span class="font-semibold">Business ?</span></p>
+                <ol  class=" custom-grey-p font-semibold list-decimal list-inside">
+                    <li class="sm:py-2">Grow Portfolio & Revenue Per Customer.</li>
+                    <li class="sm:py-2">  Streamline Credit Operations.</li>
+                    <li class="sm:py-2">  Control & Manage Risk. Increase Productivity – Less Repetitive Tasks.</li>
+                    <li class="sm:py-2">  Leverage A Single Platform.</li>
+                </ol>
+
+
+
+
+            </div>
+
+        </div>
+        <div class="mx-auto max-w-7xl grid grid-cols-2 sm:gap-x-8 sm:px-8 bg-blue-white">
+            <div class="col-span-1 flex-col justify-between my-auto">
+                <p class=" my-4 tracking-wider text-2xl sm:py-4 custom-blue-200">What’s in it for your <span class="font-semibold">Employees?</span></p>
+                <ol  class=" custom-grey-p font-semibold list-decimal list-inside">
+                    <li class="sm:py-2">Automated flows – no time-consuming processes.</li>
+                    <li class="sm:py-2">Paperless approach, with digitalized forms</li>
+                    <li class="sm:py-2">Reduced risk of error – system ensures data consistency</li>
+                    <li class="sm:py-2">  Single data entry – no need of entering data multiple times</li>
+                </ol>
+
+
+
+
+            </div>
+            <div class="col-span-1 ">
+                <img src="/images/LO4.png" alt="ladiessitting" class="object-cover object-center group-hover:opacity-75 rounded-lg my-auto">
+            </div>
+
+
+
+        </div>
+        <div class="mx-auto max-w-7xl grid grid-cols-2 sm:gap-x-8 sm:px-8 bg-blue-white">
+            <div class="col-span-1 flex-col justify-between items-center my-auto ">
+                <img src="/images/LO4.png" alt="ladiessitting" class=" h-1/2 object-cover object-center group-hover:opacity-75 rounded-lg my-auto">
+            </div>
+
+            <div class="col-span-1 flex-col justify-between my-auto">
+                <p class=" my-4 tracking-wider text-2xl sm:py-4 custom-blue-200">What’s in it for your <span class="font-semibold">Customers?</span></p>
+                <ol  class=" custom-grey-p font-semibold list-decimal list-inside">
+                    <li class="sm:py-2">Apply Anytime, Anywhere – multichannel approach.</li>
+                    <li class="sm:py-2">Complete Digital Document Flow with Digital Approvals.</li>
+                    <li class="sm:py-2">Fast pre-qualification and credit decision.</li>
+                    <li class="sm:py-2">Decision and fund disbursement within minutes for simple loans.</li>
+                </ol>
+
+
+
+
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Transform.vue"
+}
+</script>
+
+<style scoped>
+
+.custom-grey-dark{
+    color:#656464;
+}
+.custom-grey-p{
+    color:#727272;
+}
+.custom-blue-200{
+    color: #217BF4
+}
+.custom-blue-900 {
+
+    color: #0A093D
+}
+</style>
